1979c78c21bff04e4b86c26cc1660457a57b1fe0
sshar43 <ssharma2@walmartlabs.com>
2016-11-15 01:10:12 +0530

    socketconnection pulgin to handle multiple socket state

--- lib/shared/cookbooks/monitor/files/default/check_socket_connection.sh
+++ lib/shared/cookbooks/monitor/files/default/check_socket_connection.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-STATE=$1
-COUNT=`netstat -an |grep "$STATE" | wc -l`
-
-echo "count=$COUNT|count=$COUNT"
+STATE1=$1
+STATE2=$2
+STATE3=$3
+OUTPUT=$(netstat -an | awk '{print $6}' | egrep "$1|$2|$3")
+count_time_out=$(echo $OUTPUT|egrep $1| wc -w)
+count_close_wait=$(echo $OUTPUT|egrep $2| wc -w)
+count_established=$(echo $OUTPUT|egrep $3| wc -w)
+echo "count_time_out=$count_time_out|count_time_out=$count_time_out "
+echo "count_close_wait=$count_close_wait|count_close_wait=$count_close_wait "
+echo "count_established=$count_established|count_established=$count_established "
